dbe942cbf3c915e3001bf88ba7a043b2b47a9009
Bhaumik <bhaumik.21becef155@gmail.com>
2024-08-01 21:52:47 +0530

    List Component Added

---
+++ myapp/src/components/List.jsx
@@ -0,0 +1,15 @@
+function List()
+{
+    return <>
+        <h1>List of Items</h1>
+        <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 4</li>
+            <li>Item 5</li>
+        </ul>
+    </>
+}
+
+export default List;
---
+++ myapp/src/components/message.jsx
@@ -0,0 +1,9 @@
+
+
+
+function Message(){
+  
+  return <h1>Hello, I'm in.</h1>;
+  }
+  
+  export default Message;
