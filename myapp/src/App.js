ef957e28440bd7f5303f2c597f8c0208a93eac14
Bhaumik <bhaumik.21becef155@gmail.com>
2024-08-03 01:24:46 +0530

    Error occure while adding button comp

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,8 +1,11 @@
+import React from "react";
 import Message from "./components/message";
 import List from "./components/List";
+import Button from "./components/Button";
+
 function App()
 {
-  return <div><Message/><List />
+  return <div><Message/><List /><Button/>
   </div>
 }
 
---
+++ myapp/src/components/Button.jsx
@@ -0,0 +1,15 @@
+function button()
+{
+
+
+
+return
+    <>
+    <h1> Here's my button</h1>
+    <button>Click Me</button>
+    </>
+
+
+}
+
+export default button;
