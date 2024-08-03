271cc69b796987675cae53b0d3218c51251f7e6a
Bhaumik <bhaumik.21becef155@gmail.com>
2024-08-03 18:01:22 +0530

    Button Count in Console

--- myapp/src/components/Button.jsx
+++ myapp/src/components/Button.jsx
@@ -5,22 +5,17 @@ const Button = () => {
   const[count,setCount]=useState(0);
 
   function handleClick ()
-  {
-      
+  {     
     setCount (count+1);
-
-      
-
-    };
+    console.log(count);
+    
+  };
 
     return (
-      <>
       <button onClick={handleClick}>
         Click Me
-
       </button>
-      <p>Clicked {count}</p>
-      </>
+
     );
     
   };
