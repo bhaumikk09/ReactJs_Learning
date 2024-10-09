e31700ba218ba6697d49b6bddb1aac6488cfe166
bhaumikk09 <bhaumik.21becef155@gmail.com>
2024-10-09 20:36:50 +0530

    Exercise 3: Changing objects quantity on click with increment

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,24 +3,27 @@ import { useState } from "react";
 
 export default function App() {
 
-const [pizza,setPizza] = useState({
-  name : 'Paneer',
-  toppings : ['Mushroom']
+const [cart,setCart] = useState({
+  discount : .1,
+  items:[
+    {id:1,title:'Product1',quantity:1},
+    {id:2,title:'Product2',quantity:1}]
 });
 
 const handleClick = () =>
 {
-  setPizza({...pizza,toppings:[...pizza.toppings,'Cheese']})
+  setCart({...cart,items:cart.items.map(item => item.id ===1 ? {...item,quantity:item.quantity + 1 }: item)})
 }
 
 
   return (
         <div>
-          <h1>Menu Info:</h1>
-          <p>Pizza Name : {pizza.name}</p>
-          <p>Toppings :</p>
+          <h1>Cart Info:</h1>
+          {/* <p>Cart Name : {pizza.name}</p> */}
+          <p>Discount : {cart.discount * 100}%</p>
+          <h2>Items :</h2>
           <ul>
-            {pizza.toppings.map((toppings,index) => (<li key={index}>{toppings}</li>))}
+            {cart.items.map(item => (<li key={item.id}>{item.title}-Quantity : {item.quantity}</li>))}
           </ul>
 
           <button onClick={handleClick}>change</button>
