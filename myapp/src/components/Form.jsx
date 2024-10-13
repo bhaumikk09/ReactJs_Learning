e288f6fa26ea52d598f38017b079e178d15cd85a
bhaumikk09 <bhaumik.21becef155@gmail.com>
2024-10-14 02:52:34 +0530

    applying validation to form

--- myapp/src/components/Form.jsx
+++ myapp/src/components/Form.jsx
@@ -3,10 +3,10 @@ import {useForm} from 'react-hook-form'
 
 const Form =  ()=>{
 
-  const { register,handleSubmit }=useForm();
+  const { register,handleSubmit,formState : {errors} }=useForm();
   
-  const onSubmit = (data)=>{console.log(data);
-  }
+  const onSubmit = (data)=>console.log(data);
+  
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -14,8 +14,11 @@ const Form =  ()=>{
             <label htmlFor='name' className='form-lable'>
             Name :
             </label>
-            <input {...register('name')} id='name' type='text' className='form-control'>
+            <input {...register('name',{required:true,minLength:3})} id='name' type='text' className='form-control'>
             </input>
+            
+            {errors.name?.type === 'required' && <p>Name field is required.</p>}
+            {errors.name?.type === 'minLength' && <p>Name must be atleast 3 character.</p>}
         </div>
         <div className='mb-3'>
             <label htmlFor='age' className='form-lable'>
