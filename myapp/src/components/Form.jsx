4405b6df9811ef6b9ec98821a852670eec0672b8
bhaumikk09 <bhaumik.21becef155@gmail.com>
2024-10-15 00:59:01 +0530

    Zod library to simplify all validation at single place

--- myapp/src/components/Form.jsx
+++ myapp/src/components/Form.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import {useForm} from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const schema = z.object({
+  name: z.string().min(3,{message:'Name must be atleast 3 Characters.'}),
+  age: z.number({invalid_type_error:'Age field is required'}).min(18,{message:'Age must be atleast 18.'})
+});
 
 const Form =  ()=>{
 
-  const { register,handleSubmit,formState : {errors} }=useForm();
+  const { register,handleSubmit,formState : {errors} }=useForm({resolver:zodResolver(schema) });
   
   const onSubmit = (data)=>console.log(data);
   
@@ -14,18 +21,18 @@ const Form =  ()=>{
             <label htmlFor='name' className='form-lable'>
             Name :
             </label>
-            <input {...register('name',{required:true,minLength:3})} id='name' type='text' className='form-control'>
+            <input {...register('name')} id='name' type='text' className='form-control'>
             </input>
             
-            {errors.name?.type === 'required' && <p>Name field is required.</p>}
-            {errors.name?.type === 'minLength' && <p>Name must be atleast 3 character.</p>}
-        </div>
+            {errors.name && <p>{errors.name.message}</p>}
+           </div>
         <div className='mb-3'>
             <label htmlFor='age' className='form-lable'>
             Age :
             </label>
-            <input {...register('age')} id='age' type='number' className='form-control'>
+            <input {...register('age',{ valueAsNumber:true })} id='age' type='number' className='form-control'>
             </input>
+            {errors.age && <p>{errors.age.message}</p>}
         </div>
         <button className="btn" type='submit'>Submit</button>
     </form>
