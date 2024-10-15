a6d00c078a1e3a5d43a6335b105d8ca8427d8d74
bhaumikk09 <bhaumik.21becef155@gmail.com>
2024-10-16 01:42:20 +0530

    Started building Expense Tracker

---
+++ myapp/src/expense-tracker/components/ExpenseList.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ExpenseList = () => {
+  return (
+    <table>
+        <thead>
+            <tr>
+                <th>Description</th>
+                <th>Amount</th>
+                <th>Category</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+    </table>
+  )
+}
+
+export default ExpenseList
