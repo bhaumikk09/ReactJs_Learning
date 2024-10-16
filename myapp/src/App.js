0cf7d2f0a10925034a49c7a374f1ad43312e7fa2
bhaumikk09 <bhaumik.21becef155@gmail.com>
2024-10-17 00:48:12 +0530

    ExpenseList is done with props data in table

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,19 @@
-import Form from "./components/Form";
+// import Form from "./components/Form";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 
 export default function App() {
 
+  const expenses = [
+    {id : 1,description : 'aaa',amount : 10, categories : 'grocerry'},
+    {id : 2,description : 'bbb',amount : 10, categories : 'grocerry'},
+    {id : 3,description : 'ccc',amount : 10, categories : 'grocerry'},
+    {id : 4,description : 'ddd',amount : 10, categories : 'grocerry'},
+  ]
+
   return (
         <div>
-          <Form/>
+          <ExpenseList expenses={expenses} onDelete={(id)=> console.log('Delete',id)}/>
         </div>
    
    );
--- myapp/src/expense-tracker/components/ExpenseList.jsx
+++ myapp/src/expense-tracker/components/ExpenseList.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const ExpenseList = () => {
+const ExpenseList = ({expenses,onDelete}) => {
   return (
     <table>
         <thead>
@@ -13,10 +13,38 @@ const ExpenseList = () => {
             </tr>
         </thead>
         <tbody>
-
+          {expenses.map(expense => <tr key={expense.id}>
+            <td>{expense.description}</td>
+            <td>{expense.amount}</td>
+            <td>{expense.category}</td>
+            <td>
+              <button onClick={()=> onDelete(expense.id)}>Delete</button>
+            </td>
+          </tr>)}
         </tbody>
+        <tfoot>
+          <tr>
+            <td>Total</td>
+            <td>${expenses.reduce((acc,expense)=>expense.amount + acc,0 ).toFixed(2)}</td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tfoot>
     </table>
   )
 }
+
+const expense= PropTypes.shape({
+  id : PropTypes.number.isRequired,
+  description : PropTypes.string.isRequired,
+  amount : PropTypes.number.isRequired,
+  category : PropTypes.string.isRequired,
+});
+
+ExpenseList.propTypes =
+{
+  expenses : PropTypes.arrayOf(expense),
+  onDelete : PropTypes.func.isRequired,
+};
 
 export default ExpenseList
