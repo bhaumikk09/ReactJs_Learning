891a0f7f0d8ce00c1e80ead6c3b3627f10b0cc28
bhaumikk09 <bhaumik.21becef155@gmail.com>
2024-10-17 00:53:51 +0530

    Delete button logic using useState

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,19 +1,21 @@
 // import Form from "./components/Form";
+import { useState } from "react";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 
 export default function App() {
+  const [expenses,setExpenses] = useState([
+  {id : 1,description : 'aaa',amount : 10, categories : 'grocerry'},
+  {id : 2,description : 'bbb',amount : 10, categories : 'grocerry'},
+  {id : 3,description : 'ccc',amount : 10, categories : 'grocerry'},
+  {id : 4,description : 'ddd',amount : 10, categories : 'grocerry'},
+])
 
-  const expenses = [
-    {id : 1,description : 'aaa',amount : 10, categories : 'grocerry'},
-    {id : 2,description : 'bbb',amount : 10, categories : 'grocerry'},
-    {id : 3,description : 'ccc',amount : 10, categories : 'grocerry'},
-    {id : 4,description : 'ddd',amount : 10, categories : 'grocerry'},
-  ]
+
 
   return (
         <div>
-          <ExpenseList expenses={expenses} onDelete={(id)=> console.log('Delete',id)}/>
+          <ExpenseList expenses={expenses} onDelete={(id)=> setExpenses(expenses.filter(e => e.id !== id))}/>
         </div>
    
    );
